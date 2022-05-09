f7628ad93bd7ff2b16aadb3b0bce1f52d955e66f
AleixCSola <104346612+AleixCSola@users.noreply.github.com>
2022-05-09 09:03:00 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,93 +1,60 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
-#define MAX 100
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+* @brief Accio que demana pel teclat les coordenades a emplenar de la taula
+* @param errores (E) El máximo de errores permitido
+* @param filas (E) El número de filas de la tabla
+* @param cols (E) El número de columnas de la tabla
+* @param filas_usuari (E) El número de filas de la tabla
+* @param cols_usuari (E) El número de columnas de la tabla
+*/
+void demana_coordenades (int intents, int cols, int filas, int filas_usuari, int cols_usuari, int errores) //struct por referencia
+{
+    errores=3;
+    intents=4;
+    cols, filas=5;
+    if(intents>errores)
+    {
+        do
+        {
+            printf("Quina fila vols emplenar?");
+            scanf("%d", &filas_usuari);
+            printf("Quina columna vols emplenar?");
+            scanf("%d", &cols_usuari);
+            if (filas_usuari>filas)
+            {
+                printf("Tria un altre fila");
+            }
+            if(cols_usuari>cols)
+            {
+                printf("Tria un altre columna");
+            }
+        }while((filas_usuari>filas)&&(cols_usuari>cols));
+    }
+    else
+    {
+        printf("Intents esgotats");
+    }
+}
+/**
+ * @brief Accio a fer si el número es passa de columna
+* @return bool que indica si la columna del usuario es igual al del tablero
+ */
+bool columna_incorrecta(n_columnes);
+
+/**
+ * @brief Accio a fer si el número es passa de fila
+ * @return bool que indica si la fila del usuario es igual al del tablero
+ */
+bool fila_incorrecta(n_files);
+
+
 int main()
 {
-    int filas = 0, columnas = 0, errores = 0;
-    int i = 0, j = 0;
-    int tabla[MAX][MAX];
-    FILE* fichero;
-    fichero = fopen("ejemplo numeros.txt", "r");
-    
-    if(fichero == NULL)
-    {
-        printf("Error\n");
-    }
-    
-    
-    fscanf(fichero, "%d %d %d", &filas, &columnas, &errores);
-    printf("Filas: %d\t Columnas: %d\t Errores: %d\t\n", filas, columnas, errores);
-    
-    for(i = 0; i < filas; i++)
-    {
-        for(j = 0; j < columnas; j++)
-        {
-        fscanf(fichero, "%d", &tabla[i][j]);
-        printf("%d\t", tabla[i][j]);
-        }
-        printf("\n");
-    }
-    
-    
-    fclose(fichero);
-    
-    ////////////
-    /*int numeros_maximos = 0, contador = 0;
-    int indice = 0;
-    
-    numeros_maximos = (filas/2) + 1;
-    printf("%d\n", numeros_maximos);
-    
-    int tabla_columnas[filas][numeros_maximos-1]; //Definimos tabla que dice cuantos numeros poner;
-
-    for(i = 0; i < filas; i++)
-    {
-        contador = 0;
-        for(j = 0; j < columnas; j++)
-        {
-            /*contador = 0;
-            while(tabla[i][j] == 1)
-            {
-                contador++;
-                j++;
-            }
-            tabla_numeros_maximos1[indice1][indice2] = contador;
-            indice2++;*/
-            /*if(tabla[i][j] == 1)
-            {
-                contador++;
-            }
-            else if (contador > 0)
-            {
-                tabla_columnas[filas][indice] = contador;
-                contador = 0;
-                indice++;
-
-            }
-        }
-        if(contador >0)
-        {
-        tabla_columnas[i][indice] = contador;
-        contador = 0;
-        indice = 0;
-        }
-    }
-    
-    for(i = 0; i < filas; i++)
-    {
-        for(j = 0; j < numeros_maximos; j++)
-        {
-        printf("%d\t", tabla_columnas[i][j]);
-        }
-        printf("\n");
-    }*/
-    
-
+    int errores,  cols,  filas,  filas_usuari, cols_usuari,indice;
+    demana_coordenades(errores, cols, filas, filas_usuari, cols_usuari, indice);
 }
