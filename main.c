7b2e86889163ceb648116baae06934574c3bcc5f
paulaclua <101551781+paulaclua@users.noreply.github.com>
2022-05-09 09:40:48 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,93 +1,39 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
-#define MAX 100
+#include <stdlib.h>
+
+bool comprueba (int tauler, int filas_usuari, int cols_usuari)
+{
+    if (tauler[filas_usuari][cols_usuari]==1)//todo esta bien y coincide
+    {
+        comprueba = true;
+    }
+    else
+    {
+        comprueba = false;
+    }
+    return comprueba;
+}
+
+void devuelve (int intentos)
+{
+    if (comprueba==true)
+    {
+        imprime_tablero_solución;
+    }
+    else
+    {
+        imprime_tablero_solución;
+        intentos++;
+    }
+
+}
+
 int main()
 {
-    int filas = 0, columnas = 0, errores = 0;
-    int i = 0, j = 0;
-    int tabla[MAX][MAX];
-    FILE* fichero;
-    fichero = fopen("ejemplo numeros.txt", "r");
-    
-    if(fichero == NULL)
-    {
-        printf("Error\n");
-    }
-    
-    
-    fscanf(fichero, "%d %d %d", &filas, &columnas, &errores);
-    printf("Filas: %d\t Columnas: %d\t Errores: %d\t\n", filas, columnas, errores);
-    
-    for(i = 0; i < filas; i++)
-    {
-        for(j = 0; j < columnas; j++)
-        {
-        fscanf(fichero, "%d", &tabla[i][j]);
-        printf("%d\t", tabla[i][j]);
-        }
-        printf("\n");
-    }
-    
-    
-    fclose(fichero);
-    
-    ////////////
-    /*int numeros_maximos = 0, contador = 0;
-    int indice = 0;
-    
-    numeros_maximos = (filas/2) + 1;
-    printf("%d\n", numeros_maximos);
-    
-    int tabla_columnas[filas][numeros_maximos-1]; //Definimos tabla que dice cuantos numeros poner;
-
-    for(i = 0; i < filas; i++)
-    {
-        contador = 0;
-        for(j = 0; j < columnas; j++)
-        {
-            /*contador = 0;
-            while(tabla[i][j] == 1)
-            {
-                contador++;
-                j++;
-            }
-            tabla_numeros_maximos1[indice1][indice2] = contador;
-            indice2++;*/
-            /*if(tabla[i][j] == 1)
-            {
-                contador++;
-            }
-            else if (contador > 0)
-            {
-                tabla_columnas[filas][indice] = contador;
-                contador = 0;
-                indice++;
-
-            }
-        }
-        if(contador >0)
-        {
-        tabla_columnas[i][indice] = contador;
-        contador = 0;
-        indice = 0;
-        }
-    }
-    
-    for(i = 0; i < filas; i++)
-    {
-        for(j = 0; j < numeros_maximos; j++)
-        {
-        printf("%d\t", tabla_columnas[i][j]);
-        }
-        printf("\n");
-    }*/
-    
-
+    int errores = 3;
+    int intentos = 0;
+    int cols, filas, cols_usuario, filas_usuario;
+    comprueba();
+    devuelve(intentos);
+    return 0;
 }
