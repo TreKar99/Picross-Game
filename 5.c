a18c446798aa5339d8f2ee22073414fb32c7add2
paulaclua <101551781+paulaclua@users.noreply.github.com>
2022-05-16 09:05:27 +0200

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+<CodeBlocks_project_file>
+	<FileVersion major="1" minor="6" />
+	<Project>
+		<Option title="picross 16/5" />
+		<Option execution_dir="C:/Users/paula/OneDrive/Escriptori/URV/II CUADRIMESTRE/FONAMENTOS PROGRAMACION/Laboratori/PRACTICA 3/5/picross 16/" />
+		<Option pch_mode="2" />
+		<Option compiler="gcc" />
+		<Build>
+			<Target title="Debug">
+				<Option output="bin/Debug/picross 16/5" prefix_auto="1" extension_auto="1" />
+				<Option object_output="obj/Debug/" />
+				<Option type="1" />
+				<Option compiler="gcc" />
+				<Compiler>
+					<Add option="-g" />
+				</Compiler>
+			</Target>
+			<Target title="Release">
+				<Option output="bin/Release/picross 16/5" prefix_auto="1" extension_auto="1" />
+				<Option object_output="obj/Release/" />
+				<Option type="1" />
+				<Option compiler="gcc" />
+				<Compiler>
+					<Add option="-O2" />
+				</Compiler>
+				<Linker>
+					<Add option="-s" />
+				</Linker>
+			</Target>
+		</Build>
+		<Compiler>
+			<Add option="-Wall" />
+		</Compiler>
+		<Unit filename="URV/II CUADRIMESTRE/FONAMENTOS PROGRAMACION/Laboratori/PRACTICA 3/5/picross 16/main.c">
+			<Option compilerVar="CC" />
+		</Unit>
+		<Extensions>
+			<lib_finder disable_auto="1" />
+		</Extensions>
+	</Project>
+</CodeBlocks_project_file>
